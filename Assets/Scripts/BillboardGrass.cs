fcf1d23e4dbb9865f9bd6835a36beb83be7aa118
Anton <itonyemail@gmail.com>
2024-05-28 16:41:09 -0700

    gpu instancing

--- Assets/Scripts/BillboardGrass.cs
+++ Assets/Scripts/BillboardGrass.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class BillboardGrass : MonoBehaviour
 {
-    public GameObject grassPrefab; // The grass prefab with 3 intersecting quads
+    public Mesh grassMesh;
+    public Material grassMaterial;
     public int gridWidth = 100; // Width of the grid in meters
     public int gridHeight = 100; // Height of the grid in meters
     public float spacing = 1f; // Distance between each grass instance
+
+    private ComputeBuffer positionBuffer;
+    private ComputeBuffer argsBuffer;
 
     void Start()
     {
@@ -16,15 +20,43 @@ public class BillboardGrass : MonoBehaviour
     {
         Debug.Log("Initializing grass...");
 
+        int grassCount = gridWidth * gridHeight;
+        Vector4[] positions = new Vector4[grassCount];
+
         for (int x = 0; x < gridWidth; x++)
         {
             for (int z = 0; z < gridHeight; z++)
             {
-                Vector3 position = new Vector3(x * spacing, 0, z * spacing);
-                Instantiate(grassPrefab, position, Quaternion.identity, transform);
+                int index = x * gridHeight + z;
+                positions[index] = new Vector4(x * spacing, 0, z * spacing, 0);
             }
         }
 
+        positionBuffer = new ComputeBuffer(grassCount, sizeof(float) * 4);
+        positionBuffer.SetData(positions);
+        grassMaterial.SetBuffer("positionBuffer", positionBuffer);
+
+        uint[] args = new uint[5] { grassMesh.GetIndexCount(0), (uint)grassCount, 0, 0, 0 };
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        argsBuffer.SetData(args);
+
         Debug.Log("Grass initialized.");
+    }
+
+    void Update()
+    {
+        Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial, new Bounds(Vector3.zero, new Vector3(gridWidth * spacing, 10, gridHeight * spacing)), argsBuffer);
+    }
+
+    void OnDestroy()
+    {
+        if (positionBuffer != null)
+        {
+            positionBuffer.Release();
+        }
+        if (argsBuffer != null)
+        {
+            argsBuffer.Release();
+        }
     }
 }
--- Assets/Shaders/BillboardGrass.shader
+++ Assets/Shaders/BillboardGrass.shader
@@ -9,7 +9,6 @@ Shader "Unlit/BillboardGrass"
         Tags { "RenderType"="Opaque" }
         Pass
         {
-            Cull Off
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
@@ -23,39 +22,26 @@ Shader "Unlit/BillboardGrass"
                 float2 uv : TEXCOORD0;
             };
 
-            struct GrassData
-            {
-                float3 position;
-                float2 uv;
-            };
-
-            StructuredBuffer<GrassData> grassBuffer;
+            StructuredBuffer<float4> positionBuffer;
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
 
             v2f vert (appdata_full v, uint id : SV_InstanceID)
             {
-                GrassData data = grassBuffer[id];
+                float4 position = positionBuffer[id];
 
                 v2f o;
-                float3 billboardPos = data.position;
-
-                // For simplicity, just place the quad directly
-                float3 quadPos = v.vertex + billboardPos;
+                float3 quadPos = v.vertex + position.xyz;
                 o.pos = UnityObjectToClipPos(float4(quadPos, 1));
 
-                // Pass UVs from the vertex data
-                o.uv = v.texcoord.xy;
+                o.uv = TRANSFORM_TEX(v.texcoord, _MainTex);
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                fixed4 texColor = tex2D(_MainTex, i.uv);
-                // Perform alpha clipping
-                if (texColor.a < 0.5) discard; // Adjust the threshold as needed
-                return texColor;
+                return tex2D(_MainTex, i.uv);
             }
             ENDCG
         }
