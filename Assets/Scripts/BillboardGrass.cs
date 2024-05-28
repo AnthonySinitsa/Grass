590699bbdfb123b66dbc15a4ddb22e007f462355
Anton <itonyemail@gmail.com>
2024-05-27 17:16:15 -0700

    updated to have displacement strength

--- Assets/Scripts/BillboardGrass.cs
+++ Assets/Scripts/BillboardGrass.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BillboardGrass : MonoBehaviour {
     public int resolution = 100;
     public int scale = 1;
+    public float displacementStrength = 200.0f;
     public Material grassMaterial;
     public Mesh grassMesh;
     public Texture heightMap;
@@ -18,12 +19,13 @@ public class BillboardGrass : MonoBehaviour {
     private struct GrassData {
         public Vector4 position;
         public Vector2 uv;
+        public float displacement;
     }
 
     void Start() {
         resolution *= scale;
         initializeGrassShader = Resources.Load<ComputeShader>("GrassPoint");
-        grassDataBuffer = new ComputeBuffer(resolution * resolution, sizeof(float) * 6);
+        grassDataBuffer = new ComputeBuffer(resolution * resolution, sizeof(float) * 7);
         argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
 
         updateGrassBuffer();
@@ -34,6 +36,7 @@ public class BillboardGrass : MonoBehaviour {
         initializeGrassShader.SetInt("_Scale", scale);
         initializeGrassShader.SetBuffer(0, "_GrassDataBuffer", grassDataBuffer);
         initializeGrassShader.SetTexture(0, "_HeightMap", heightMap);
+        initializeGrassShader.SetFloat("_DisplacementStrength", displacementStrength);
         initializeGrassShader.Dispatch(0, Mathf.CeilToInt(resolution / 8.0f), Mathf.CeilToInt(resolution / 8.0f), 1);
 
         uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
@@ -45,24 +48,29 @@ public class BillboardGrass : MonoBehaviour {
 
         grassMaterial.SetBuffer("positionBuffer", grassDataBuffer);
         grassMaterial.SetFloat("_Rotation", 0.0f);
+        grassMaterial.SetFloat("_DisplacementStrength", displacementStrength);
         grassMaterial2 = new Material(grassMaterial);
         grassMaterial2.SetBuffer("positionBuffer", grassDataBuffer);
         grassMaterial2.SetFloat("_Rotation", 50.0f);
+        grassMaterial2.SetFloat("_DisplacementStrength", displacementStrength);
         grassMaterial3 = new Material(grassMaterial);
         grassMaterial3.SetBuffer("positionBuffer", grassDataBuffer);
         grassMaterial3.SetFloat("_Rotation", -50.0f);
+        grassMaterial3.SetFloat("_DisplacementStrength", displacementStrength);
     }
 
     void Update() {
         grassMaterial.SetBuffer("positionBuffer", grassDataBuffer);
         grassMaterial.SetFloat("_Rotation", 0.0f);
+        grassMaterial.SetFloat("_DisplacementStrength", displacementStrength);
         grassMaterial2 = new Material(grassMaterial);
         grassMaterial2.SetBuffer("positionBuffer", grassDataBuffer);
         grassMaterial2.SetFloat("_Rotation", 50.0f);
+        grassMaterial2.SetFloat("_DisplacementStrength", displacementStrength);
         grassMaterial3 = new Material(grassMaterial);
         grassMaterial3.SetBuffer("positionBuffer", grassDataBuffer);
         grassMaterial3.SetFloat("_Rotation", -50.0f);
-
+        grassMaterial3.SetFloat("_DisplacementStrength", displacementStrength);
         Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial, new Bounds(Vector3.zero, new Vector3(500.0f, 200.0f, 500.0f)), argsBuffer);
         Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial2, new Bounds(Vector3.zero, new Vector3(500.0f, 200.0f, 500.0f)), argsBuffer);
         Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial3, new Bounds(Vector3.zero, new Vector3(500.0f, 200.0f, 500.0f)), argsBuffer);
