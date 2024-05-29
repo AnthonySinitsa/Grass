1c266063a5f7725dc99b2828d1fb85e3c28822a4
Anton <itonyemail@gmail.com>
2024-05-28 20:32:40 -0700

    comments

--- Assets/Scripts/BillboardGrass.cs
+++ Assets/Scripts/BillboardGrass.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class BillboardGrass : MonoBehaviour
 {
-    public Mesh grassMesh;
-    public Material grassMaterial;
+    public Mesh grassMesh; // Reference to the grass mesh (with 3 intersecting quads)
+    public Material grassMaterial; // Reference to the grass material using the shader
     public int gridWidth = 100; // Width of the grid in meters
     public int gridHeight = 100; // Height of the grid in meters
     public float spacing = 1f; // Distance between each grass instance
 
-    private ComputeBuffer positionBuffer;
-    private ComputeBuffer argsBuffer;
+    private ComputeBuffer positionBuffer; // Buffer to hold position data for each grass instance
+    private ComputeBuffer argsBuffer; // Buffer to hold draw arguments for indirect rendering
 
     void Start()
     {
@@ -20,9 +20,10 @@ public class BillboardGrass : MonoBehaviour
     {
         Debug.Log("Initializing grass...");
 
-        int grassCount = gridWidth * gridHeight;
-        Vector4[] positions = new Vector4[grassCount];
+        int grassCount = gridWidth * gridHeight; // Total number of grass instances
+        Vector4[] positions = new Vector4[grassCount]; // Array to hold the positions
 
+        // Fill the positions array with the positions of each grass instance
         for (int x = 0; x < gridWidth; x++)
         {
             for (int z = 0; z < gridHeight; z++)
@@ -32,10 +33,12 @@ public class BillboardGrass : MonoBehaviour
             }
         }
 
+        // Create and fill the position buffer
         positionBuffer = new ComputeBuffer(grassCount, sizeof(float) * 4);
         positionBuffer.SetData(positions);
         grassMaterial.SetBuffer("positionBuffer", positionBuffer);
 
+        // Set up the draw arguments buffer
         uint[] args = new uint[5] { grassMesh.GetIndexCount(0), (uint)grassCount, 0, 0, 0 };
         argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
         argsBuffer.SetData(args);
@@ -45,11 +48,13 @@ public class BillboardGrass : MonoBehaviour
 
     void Update()
     {
+        // Draw the grass instances using GPU instancing
         Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial, new Bounds(Vector3.zero, new Vector3(gridWidth * spacing, 10, gridHeight * spacing)), argsBuffer);
     }
 
     void OnDestroy()
     {
+        // Release the buffers to free up GPU memory
         if (positionBuffer != null)
         {
             positionBuffer.Release();
